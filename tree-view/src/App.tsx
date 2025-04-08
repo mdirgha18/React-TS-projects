97d7b997d3ef26c79af021a6a2a9a6ec157530de
mdirgha18 <dirgha.mankad702@gmail.com>
2025-04-08 16:23:00 +0530

    Add files via upload

---
+++ tree-view/src/App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import TreeView from "./TreeView";
+
+const treeData = [
+  {
+    label: "Root",
+    children: [
+      {
+        label: "Child 1",
+        children: [{ label: "Grandchild 1.1" }, { label: "Grandchild 1.2" }],
+      },
+      {
+        label: "Child 2",
+        children: [{ label: "Grandchild 2.1" }],
+      },
+    ],
+  },
+];
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h2>Tree View Example</h2>
+      <TreeView data={treeData} />
+    </div>
+  );
+};
+
+export default App;
