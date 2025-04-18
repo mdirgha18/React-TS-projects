ce8110e2369e10fb3af496294e630fefb14671a1
mdirgha18 <dirgha.mankad702@gmail.com>
2025-04-18 16:00:22 +0530

    Add files via upload

---
+++ 9-tic-tac-toe/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TicTacToe from "./TicTacToe";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <TicTacToe />
+    </div>
+  );
+};
+
+export default App;
