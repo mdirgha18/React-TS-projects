7247fd4ee987039934e2a6dbfa0fecd3888a1c62
mdirgha18 <dirgha.mankad702@gmail.com>
2025-04-19 21:28:18 +0530

    Delete Screenshot and Annotation tool - Project 14 directory

--- Screenshot and Annotation tool - Project 14/src/hooks/useScreenshot.ts
+++
@@ -1,9 +0,0 @@
-import html2canvas from "html2canvas";
-
-export async function useScreenshot(id = "screenshot-area") {
-  const element = document.getElementById(id);
-  if (!element) return null;
-
-  const canvas = await html2canvas(element);
-  return canvas.toDataURL("image/png");
-}
