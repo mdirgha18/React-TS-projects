e42c90c366bd4614338c123d6d5c3173fe3520f3
mdirgha18 <dirgha.mankad702@gmail.com>
2025-05-12 13:05:01 +0530

    Update Pixel.tsx

--- 20-pixel-art-creator/src/components/Pixel.tsx
+++ 20-pixel-art-creator/src/components/Pixel.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 
+// Define the Props type to specify the expected properties for the Pixel component
 type Props = {
-  color: string;
-  onClick: () => void;
+  color: string; // The background color of the pixel passed as a prop
+  onClick: () => void; // A callback function that will be triggered when the pixel is clicked
 };
 
+// Pixel functional component that receives color and onClick as props
 const Pixel: React.FC<Props> = ({ color, onClick }) => {
   return (
     <div
-      className="pixel"
-      style={{ backgroundColor: color }}
-      onClick={onClick}
+      className="pixel" // CSS class for styling the pixel
+      style={{ backgroundColor: color }} // Set the background color of the pixel based on the 'color' prop
+      onClick={onClick} // Trigger the onClick callback when the pixel is clicked
     />
   );
 };
 
+// Export the Pixel component for use in other parts of the app
 export default Pixel;
