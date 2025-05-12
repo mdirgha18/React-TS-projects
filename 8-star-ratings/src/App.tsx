a804435c9dbad0d9509b2fb5c4d33318b66dfd7e
mdirgha18 <dirgha.mankad702@gmail.com>
2025-05-12 13:50:20 +0530

    Update App.tsx

--- 8-star-ratings/src/App.tsx
+++ 8-star-ratings/src/App.tsx
@@ -1,14 +1,16 @@
 import React from "react";
-import StarRating from "./StarRating";
+import StarRating from "./StarRating"; // Importing the StarRating component
 
 const App: React.FC = () => {
+  // Function to handle the rating change
   const handleRatingChange = (rating: number) => {
-    console.log("Rated:", rating);
+    console.log("Rated:", rating); // Log the selected rating
   };
 
   return (
     <div style={{ padding: "40px" }}>
       <h1>Rate this product</h1>
+      {/* StarRating component with props for total stars and the rating change handler */}
       <StarRating totalStars={5} onRatingChange={handleRatingChange} />
     </div>
   );
