f8d1de027628eff195b1dc1dde1a325130897558
mdirgha18 <dirgha.mankad702@gmail.com>
2025-05-12 13:23:48 +0530

    Update Paragraph.tsx

--- 24-text-to-speech-reading-paragraph/src/components/Paragraph.tsx
+++ 24-text-to-speech-reading-paragraph/src/components/Paragraph.tsx
@@ -1,15 +1,19 @@
+// Importing React to use JSX and hooks in the component
 import React from "react";
 
+// Defining the types for the props that the Paragraph component will receive
 interface ParagraphProps {
-  paragraph: string;
+  paragraph: string; // The text content to be displayed inside the paragraph
 }
 
+// Defining the functional component with its props
 const Paragraph: React.FC<ParagraphProps> = ({ paragraph }) => {
   return (
-    <div className="paragraph">
-      <p>{paragraph}</p>
+    <div className="paragraph"> {/* Wrapper div for the paragraph */}
+      <p>{paragraph}</p> {/* Rendering the paragraph text inside <p> tag */}
     </div>
   );
 };
 
+// Exporting the Paragraph component to be used elsewhere in the application
 export default Paragraph;
