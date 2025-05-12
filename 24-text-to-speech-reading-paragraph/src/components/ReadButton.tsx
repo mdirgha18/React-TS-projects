c5742e675d6d615255ab96ae02eb7ffa5e7911db
mdirgha18 <dirgha.mankad702@gmail.com>
2025-05-12 13:24:19 +0530

    Update ReadButton.tsx

--- 24-text-to-speech-reading-paragraph/src/components/ReadButton.tsx
+++ 24-text-to-speech-reading-paragraph/src/components/ReadButton.tsx
@@ -1,16 +1,20 @@
+// Importing React to use JSX and hooks in the component
 import React from "react";
 
-// Button component to trigger the speech synthesis
+// Defining the types for the props that the ReadButton component will receive
 interface ReadButtonProps {
-  onClick: () => void;
+  onClick: () => void; // The function to be executed when the button is clicked
 }
 
+// Defining the functional component with its props
 const ReadButton: React.FC<ReadButtonProps> = ({ onClick }) => {
   return (
-    <div className="read-button">
+    <div className="read-button"> {/* Wrapper div for the button */}
+      {/* The button element which triggers the onClick function when clicked */}
       <button onClick={onClick}>Read Paragraph Aloud</button>
     </div>
   );
 };
 
+// Exporting the ReadButton component to be used elsewhere in the application
 export default ReadButton;
