a0b4b4002f644b29932b8f6b32024a477039a195
mdirgha18 <dirgha.mankad702@gmail.com>
2025-05-12 12:59:11 +0530

    Update note.ts

--- 17. Sticky Noes/src/types/note.ts
+++ 17. Sticky Noes/src/types/note.ts
@@ -1,7 +1,8 @@
+// Interface defining the structure of a sticky note
 export interface StickyNoteData {
-  id: string;
-  text: string;
-  x: number;
-  y: number;
-  color: string;
+  id: string; // Unique identifier for each sticky note
+  text: string; // The text content of the sticky note
+  x: number; // The X-coordinate for the sticky note's position
+  y: number; // The Y-coordinate for the sticky note's position
+  color: string; // The background color of the sticky note
 }
