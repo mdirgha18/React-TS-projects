c0ae455dbe2fb010bcb83e26e5f767d517684602
mdirgha18 <dirgha.mankad702@gmail.com>
2025-06-05 16:24:35 +0530

    Update App.tsx

--- 64-guess/src/App.tsx
+++ 64-guess/src/App.tsx
@@ -1,53 +1,31 @@
-import React, { Component, ChangeEvent } from "react";
+import React, { useState, ChangeEvent } from "react";
 import Result from "./Result";
 import "./styles.css";
 
-type AppProps = {
-  secret?: number;
-};
+const App: React.FC = () => {
+  const [term, setTerm] = useState<string>("");
+  const [secret] = useState<number>(Math.floor(Math.random() * 20) + 1);
 
-type AppState = {
-  term: string;
-};
-
-class App extends Component<AppProps, AppState> {
-  static defaultProps: AppProps = {
-    secret: Math.floor(Math.random() * 20) + 1,
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTerm(e.target.value);
   };
 
-  constructor(props: AppProps) {
-    super(props);
-    this.state = {
-      term: "",
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event: ChangeEvent<HTMLInputElement>) {
-    const { name, value } = event.target;
-    this.setState({ [name]: value } as Pick<AppState, keyof AppState>);
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <div className="head">
-          <label htmlFor="term">Guess the number between 1 to 20</label>
-        </div>
-
-        <input
-          id="term"
-          type="text"
-          name="term"
-          value={this.state.term}
-          onChange={this.handleChange}
-        />
-
-        <Result term={this.state.term} secretNum={this.props.secret!} />
+  return (
+    <div className="container">
+      <div className="head">
+        <label htmlFor="term">Guess the number between 1 and 20</label>
       </div>
-    );
-  }
-}
+      <input
+        id="term"
+        type="text"
+        name="term"
+        value={term}
+        onChange={handleChange}
+        placeholder="Enter your guess"
+      />
+      <Result term={term} secretNum={secret} />
+    </div>
+  );
+};
 
 export default App;
